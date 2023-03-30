f6e235eadaed970346282bb711efc329f3db3e92
haiiron Mac <kirogroryhv@gmail.com>
2023-03-30 16:25:50 +0900

    엥?

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-var input = document.getElementById('input');
-const result = document.getElementById('result');
-
 function changeValue()
 {
-  result.innterText = "hi";
+  const input = document.getElementById('input');
+  const result = document.getElementById("result");
+  var output = input.value * 1800;
+  result.innerText = output;
 }
