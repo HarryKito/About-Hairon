d07aced7113b87f6456489672fee17bd6687d347
harrykito <korhary@gmail.com>
2024-04-20 02:30:28 +0900

    new design

--- script/script.js
+++
@@ -1,7 +0,0 @@
-function changeValue()
-{
-  const input = document.getElementById('input');
-  const result = document.getElementById("result");
-  var output = input.value * 1800;
-  result.innerText = output;
-}
